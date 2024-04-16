b4e98e82ee23d5f101f87aa5727f778c90d5cb4d
progress0407 <progress0407@gmail.com>
2024-04-16 17:28:20 +0900

    리액트 핵심 내용 복습 및 뼈대 코드 작성

--- README.md
+++ README.md
@@ -2,15 +2,30 @@
 
 ## 메모
 
+`npm start`: 로컬 환경에서 실행. 빠르게 개발하고 반응을 즉각적으로 확인할 수 있음
+
 `npx create-ract-app .` : 현재 경로에 리액트 샘플 프로젝트 생성
 
 `npm run build`: npm 모듈 실행
 
 `npx serve -s build` build 디렉터리를 배포
 
+`key..`: html을 render하는 반복문 안에서 고유한 key란 이름의 prop을 주면 된다 (리액트가 자동으로 생성하면서 추적을 해야 한다, 성능을 높이고 정확한 동작에 필요)
+
+`prop`: html을 render할때, 부모 html 조각에서 자식 html 조각에게 넘기는 속성 값들
+
+`훅`: 리액트에서 제공하는 빌트인 함수. ex) useState
+
+`setXXX` 실행시, 해당 훅을 포함한 컴포넌트가 다시 렌더링된다
+
+`setState`를 할 때, 원시 타입과 달리 Reference타입은 주소에 해당하는 객체 자체를 바꿔줘야 한다. (깊은 복사).
+Java로 생각해보면 동등성이 아닌 동일성 비교를 기준으로 상태 변경 여부를 확인하는 것으로 보인다.
+
+`state`는 read-only이다. setState를 통해 state를 변경해야 한다
+
+`onChange`: html과 다르게 상태가 변경될 때마다 다시 렌더링한다
+
 ## 오답 노트
 
 ### npx 명령어 실행시 오류
 `npx create-react-app . `를 실행해서 npm 관련 오류가 날 경우, %AppData%에 npm 디렉터리를 만들어서 해결
-
-
--- src/App.js
+++ src/App.js
@@ -1,23 +1,187 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function Header(props) {
+  return <header>
+  <h1><a href="/" onClick={(event)=>{
+    console.log(event);
+    event.preventDefault(); // // a태그의 이벤트(href)를 막는다
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
+</header>
+}
+
+function Nav(props) {
+  const lis = []
+  const topics = props.topics
+
+  for(let i=0; i < topics.length; i++) {
+    let topic = topics[i];
+    lis.push(
+      <li key={topic.id}>
+        <a id={topic.id} href={'/read/' + topic.id} onClick={(event, id)=>{
+          event.preventDefault();
+          props.onChangeMode(Number(event.target.id));  // event를 발생시킨 태그를 가리킴 -> a 태그
+        }}>{topic.title}</a>
+      </li>
+    );
+  }
+
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(event)=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="제목" /></p>
+      <p><textarea name="body" placeholder="본문"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+
+function Update(props) {
+
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  console.log('업데이트 이벤트 발생')
+  props.title = 'some-value'
+
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={(event)=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type="text" name="title" value={title} onChange={(event)=>{
+        console.log(event.target.value)
+        setTitle(event.target.value)
+      }}/></p>
+      <p><textarea name="body" value={body} onChange={(event)=>{
+        setBody(event.target.body)
+      }}></textarea></p>
+      <p><input type="submit" value="Update"></input></p>
+    </form>
+  </article>
+}
 
 function App() {
+  console.log('App 렌더링')
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[0];
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]);
+
+  let content = null;
+  let contextControl = null;
+
+  if(mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }
+  else if(mode === 'READ') {
+    let title = null;
+    let body = null;
+
+    for(let i=0; i < topics.length; i++) {
+      let topic = topics[i];
+      if(topic.id === id) {
+        title = topic.title;
+        body = topic.body;
+      }
+    }
+
+    content = <Article title={title} body={body}></Article>
+
+    contextControl = <>
+    <li>
+      <a href={'/update/'+id} onClick={event=>{
+        event.preventDefault();
+        setMode('UPDATE');
+      }}>Update</a>
+    </li>
+    <li>
+      <a href={'/delete/'+id} onClick={event=>{
+        event.preventDefault();
+        setMode('DELETE');
+      }}>Delete</a>
+    </li>
+    </>
+  }
+  else if(mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopics = [...topics];
+      const newTopic = {id: nextId, title:_title, body:_body}
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
+  } 
+  else if(mode  === 'UPDATE') {
+    let foundTopic = topics.find(item => item.id === id)
+    content = <Update title={foundTopic.title} body={foundTopic.body} onUpdate={(_title, _body)=>{
+      foundTopic.title = _title
+      foundTopic.body = _body
+      const newTopics = [...topics];
+      setTopics(newTopics)
+      setMode('READ')
+    }}></Update>
+  }
+  else if(mode  === 'DELETE') {
+    let topicsAfterDeleted = topics.filter(topic=> topic.id !== id)
+    setTopics(topicsAfterDeleted)
+    setMode('WELCOME')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="REACT" onChangeMode={()=>{
+        alert('Hello, Welcome!');
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+      }}></Nav>
+      {content}
+      <ul>
+        <li>
+          <a href="/create" onClick={(event)=>{
+            event.preventDefault();
+            setMode('CREATE');
+          }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
+      
     </div>
   );
 }
